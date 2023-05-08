ca68df06cd338c28b6bc2e4b76c9a78a2a36249c
shaxboz20072007 <shaxbozniyozqulov000@gmail.com>
2023-05-07 19:03:54 -0700

    tarjimon

---
+++ script.js
@@ -0,0 +1,81 @@
+let a = prompt("meva  nomini kiriting")
+
+switch (a) {
+    case "olma":
+        alert("apple" )
+        break;
+    case "gilos":
+        alert("cherry")
+        break;
+    case "o'rik":
+        alert("epricot")
+        break;
+    case "qulupnay":
+        alert("strawberry")
+        break;
+    case "tarvuz":
+        alert("watermelon")
+        break;
+    case "olcha":
+        alert("cherry")
+        break;
+    case "anjir":
+        alert("fig")
+        break;
+    case "avocado":
+        alert("avocado")
+        break;
+    case "banan":
+        alert("banana")
+        break;
+    case "qora smorodina":
+        alert("blackcurrant")
+        break;
+    case "buta mevasi":
+        alert("blueberry")
+        break;
+    case "kakos":
+        alert("coconut")
+        break;
+    case "uzum":
+        alert("grape")
+        break;
+    case "kivi":
+        alert("kivi")
+        break;
+    case "lemon":
+        alert("lemon")
+        break;
+    case "laym":
+        alert("lime")
+        break;
+    case "mango":
+        alert("mango")
+        break;
+    case "shoftoli":
+        alert("peach")
+        break;
+    case "mandarin":
+        alert("orange")
+        break;
+    case "nok":
+        alert("pear")
+        break;
+    case "olxo'ri":
+        alert("plum")
+        break;
+    case "behi":
+        alert("quence")
+        break;
+    case "qovun":
+        alert("melon")
+        break;
+    case "xurmo":
+        alert("dates")
+        break;
+        case "ananas":
+            alert("ananas")
+    default:
+        alert("meva nomini tug'ri kiriting")
+        break;
+}
